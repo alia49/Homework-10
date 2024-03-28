35f8e910f79400111019b3fe36716cf5dc9ea6ce
alia49 <aaltimimi01@gmail.com>
2024-03-28 15:30:23 -0700

    the real branch

---
+++ homework10.py
@@ -0,0 +1,36 @@
+from collections import deque
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: 'Optional[TreeNode]') -> 'List[List[int]]':
+        if not root:
+            return None
+        
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            level_size = len(queue)
+            level = []
+            
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            result.append(level)
+        
+        return result
+
+array = [4, 3, 8, 1, 5, 9]
+
+for index, element in enumerate(array):
+    print(f"Index: {index}, Value: {element}")
+
