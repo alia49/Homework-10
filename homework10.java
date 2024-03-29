d36ff46a5d783231e5a0780b7c27f6b67989d46c
alia49 <aaltimimi01@gmail.com>
2024-03-28 22:56:39 -0700

    The rest of the Homework done in Java

--- homework10.java
+++ homework10.java
@@ -1,1 +1,55 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class homework10{
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new ArrayList<>();
+            for(int i = 0; i < levelNum; i++) {
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            result.add(subList);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Constructing a simple tree:
+        //        4
+        //       / \
+        //      3   8
+        //     /|   \
+        //    1 5    9
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(9);
+
+        List<List<Integer>> result = levelOrder(root);
+
+        System.out.println(result);
+    }
+}
